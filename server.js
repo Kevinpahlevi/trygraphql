6b0a5bd2064378a183698c5e94bb6a322f27618e
Kevin Pahlevi <kevinplevi@gmail.com>
2019-10-01 13:11:15 +0700

    2

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ var server = new ApolloServer({typeDefs,resolvers})
 var app = express();
 server.applyMiddleware({ app, path:'/graphql' });
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   // console.log('Running a GraphQL API server at localhost:4000/graphql');
-});
+})
